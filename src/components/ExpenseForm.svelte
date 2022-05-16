156f8baa0454140a5d7f77407326cd3e7c8b0c84
Nicholas Winters <nicholas.d.winters@gmail.com>
2022-05-15 19:01:45 -0500

    added add expense functionality

--- src/components/ExpenseForm.svelte
+++ src/components/ExpenseForm.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
   import Title from './Title.svelte';
+  import type { TypeExpense } from '../types';
+import { createEventDispatcher } from 'svelte';
+
+  let name = '';
+  let amount = 0;
+  let id = Math.floor(Math.random() * 1000);
+  let dispatch = createEventDispatcher();
+  let submitForm = () => {
+    let expense: TypeExpense = { id, name, amount };
+    dispatch('add', {expense: expense})
+  }
 </script>
 
 <section class="form">
   <Title title="add expense" />
+  <form class="expense-form">
+    <div class="form-control">
+      <label for="name">name</label>
+      <input type="text" bind:value={name} id="name">
+    </div>
+    <div class="form-control">
+      <label for="amount">amount</label>
+      <input type="number" bind:value={amount} id="amount">
+    </div>
+    <p class="form-empty">
+      Please fill out all form fields
+    </p>
+    <button type="button" class="btn btn-block" on:click={submitForm}>add expense</button>
+    <button type="button" class="close-btn"><i class="fas fa-times"></i>close</button>
+  </form>
 </section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
   import ExpenseForm from '../components/ExpenseForm.svelte';
 
   let expenses = [...expensesData];
-  
+  $: expenses;
   // dispatching events - create function in parent component (removeExpense), takes an event, contains 'details' which will hold our info that we pass through, use on:remove on the child component, use event forwarding through all other components using on:remove, and then in final child componet, Expense.svelte, create event dispatcher, and dispatch the remove event, passing through the id we need
 
   let removeExpense = (event: CustomEvent) => {
@@ -16,11 +16,16 @@
     expenses = [];
   }
 
+  let addExpense = (event: CustomEvent) => {
+    expenses = [event.detail.expense, ...expenses];
+
+  }
+
 </script>
 
 <Navbar />
 <main class="content">
-  <ExpenseForm />
+  <ExpenseForm on:add={addExpense} />
   <ExpensesList {expenses} on:remove={removeExpense} />
   <button on:click={clearExpenses} type='button' class="btn btn-primary btn-block">clear expenses</button>
 </main>
